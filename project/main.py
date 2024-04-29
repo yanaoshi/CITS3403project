48afc2e8ba4747f280fc31cba2c0a739da1d9793
Fred Leman <freddieleman1@gmail.com>
2024-04-29 15:42:47 +0800

    Improving login/signup functions

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from .models import Reqs
+# from .models import Users for use in profile functions
+# import reCaptcha
 from . import db
 
 main = Blueprint('main', __name__)
