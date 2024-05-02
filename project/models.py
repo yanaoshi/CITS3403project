7a3062465f5569f9f8a516a1303521718cc9f70b
boongus2 <162426336+boongus2@users.noreply.github.com>
2024-05-02 23:34:05 +0800

    Update models.py
    Implemented a database for comment section to reply to the request

--- project/models.py
+++ project/models.py
@@ -15,3 +15,10 @@ class Reqs(db.Model):
     poster = db.Column(db.String(100))
     image = db.Column(db.String(100), unique = True)
     time_created = db.Column(TIMESTAMP)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(1000))
+    req_id = db.Column(db.Integer, db.ForeignKey('reqs.id'))
+    commenter_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    time_created = db.Column(TIMESTAMP)
